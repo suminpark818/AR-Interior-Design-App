6d744c13762988562513dcf03972988bb7a3dd64
yangnem0 <142515171+yangnem0@users.noreply.github.com>
2025-09-24 00:04:42 +0900

    fix: update PlaneAnchor scene and prefab, add missing meta files

--- Assets/Yangnem/PlaneAnchor.cs
+++ Assets/Yangnem/PlaneAnchor.cs
@@ -1,33 +1,53 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-using System.Collections.Generic;
 
-public class PlaceAnchor : MonoBehaviour
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaneAnchor : MonoBehaviour
 {
-    public GameObject objectPrefab;
-    private ARRaycastManager raycastManager;
-    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    [SerializeField] private GameObject cubePrefab;
 
-    void Start()
+    private ARRaycastManager raycastManager;
+    private ARAnchorManager anchorManager;
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Awake()
     {
         raycastManager = GetComponent<ARRaycastManager>();
+        anchorManager = GetComponent<ARAnchorManager>();
     }
 
     void Update()
     {
+        Debug.Log("Touch count: " + Input.touchCount);
+
         if (Input.touchCount > 0)
-        {
+{
             Touch touch = Input.GetTouch(0);
 
             if (touch.phase == TouchPhase.Began)
             {
+                Debug.Log("Touch detected at: " + touch.position);
+
                 if (raycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
                 {
-                    var hitPose = hits[0].pose;
-                    Instantiate(objectPrefab, hitPose.position, hitPose.rotation);
+                    Pose hitPose = hits[0].pose;
+                    Debug.Log("Plane hit at: " + hitPose.position);
+
+                    var spawnedObj = Instantiate(cubePrefab, hitPose.position, hitPose.rotation);
+
+                    if (spawnedObj.GetComponent<ARAnchor>() == null)
+                    {
+                        spawnedObj.AddComponent<ARAnchor>();
+                    }
+                }
+                else
+                {
+                    Debug.Log("Raycast did not hit any plane.");
                 }
             }
         }
+
     }
 }
